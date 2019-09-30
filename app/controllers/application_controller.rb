ec08b416c2d4fbdf169491e293ed34814eae5bab
tscotce <tscotce@gmail.com>
2019-09-29 22:31:26 -0400

    added helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,32 @@ class ApplicationController < Sinatra::Base
     "hello"
   end
 
+  helpers do
+
+    def login(email, password)
+      #check if user with this email actually exits
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(password)
+        #if so, set the session
+        session[:email] = user.email
+      #otherwise, redirect
+      else redirect to '/login'
+      end
+    end
+
+    def current_user
+      # find the current user by email in the session hash
+      @current_user ||= User.find_by(email: session[:email]) if session[:email]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def logout
+      # clear session to log out the user
+      session.clear
+    end
+  end
+
 end
