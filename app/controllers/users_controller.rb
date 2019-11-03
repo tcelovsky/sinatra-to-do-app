dc1c68b320d8f459ee0733cd02e5c861f822df8b
tscotce <tscotce@gmail.com>
2019-11-03 17:15:49 -0500

    added methods to tasks controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,25 @@
 class UsersController < ApplicationController
 
   get '/login' do
-    erb 'users/login'
+    erb :'users/login'
   end
 
   post '/login' do
-    login
-    erb :"tasks/index"
+    login(email: params[:email], password: params[:password])
+    erb :'tasks/index'
   end
 
   get '/signup' do
-    erb 'users/signup'
+    erb :'users/signup'
   end
 
   post '/signup' do
     if params[:name].present? && params[:email].present? && params[:password].present?
       @user = User.create(name: params[:name], email: params[:email], password: params[:password])
       session[:id] = @user.id
-      login
-    erb :"tasks/index"
+      login(email: params[:email], password: params[:password])
+    end
+    erb :'tasks/index'
   end
 
   get '/logout' do
